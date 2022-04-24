c6172fc1c0d579b796c106f144777716e0636af4
malickalex1975 <91604689+malickalex1975@users.noreply.github.com>
2022-04-24 23:16:51 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -23,7 +23,7 @@ function dateSample( sampleActivity ) {
     let k=0.693/HALF_LIFE_PERIOD;
      sampleActivity=parseFloat(sampleActivity);
     
-  let t=Math.ceil(Math.log(MODERN_ACTIVIYY/sampleActivity)/k);
+  let t=Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/k);
   }
   return t
 }
